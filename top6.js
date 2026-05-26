13924ef97a367d83f3424464ac939236eabcde58
TotalKaos2006 <bjorn.selder@student.gildeopleidingen.nl>
2026-05-26 11:46:18 +0200

    Add files via upload

---
+++ top6.js
@@ -0,0 +1,20 @@
+// Opdracht 1
+const songs = [
+  "Blinding Lights - The Weeknd",
+  "Shape of You - Ed Sheeran",
+  "Levitating - Dua Lipa",
+  "Stay - Justin Bieber",
+  "Peaches - Justin Bieber",
+  "Good 4 U - Olivia Rodrigo"
+];
+
+// Opdracht 2
+console.log(songs[0]);
+
+// Opdracht 3
+console.log(songs[2]);
+
+// Opdracht 4// Opdracht 4
+songs.forEach(function(song) {
+  console.log(song);
+});
